5cc1ca7734beec7cd3512517fa9ca80d4d232eb2
Ilya Poklitar <vaikus.gm@gmail.com>
2022-04-19 16:32:00 +1100

    add express-handlebars

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const express = require('express');
+//const path = require('path');
+const exphbs = require('express-handlebars');
+
 const app = express();
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+// Так было до подключения express-handlebars
+// app.get('/', (req, res) => {   
+//     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// });
+
+app.get('/', (req, res) => {   
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 const PORT = process.env.PORT || 3000;
 
